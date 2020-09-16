6e3a554e911378848124ebb831d0b13786bf7958
T. Rahaman <tanvirtex@gmail.com>
2020-09-17 05:25:10 +0600

    bsc_omgswap_init

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/omgswap/public-assets/master/tokenlist/tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'defi.cmc.eth',
+  'https://raw.githubusercontent.com/omgswap/public-assets/master/tokenlist/tokenlist.json'
+  ]
